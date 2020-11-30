409af3ba546c2db3a38ade9aa530be1168d5d92f
FinallyDead <72553866+FinallyDead@users.noreply.github.com>
2020-11-30 22:05:04 +0800

    Add files via upload

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -7,12 +7,48 @@
       >
     </network>
 <br/><button @click="addNewTask">Add new Tasks.</button><br/>
-<br/><button @click="deleteNode">Delete selected node.</button><br/><br/>
-<button @click="editNode" class="editSelectedNode">Edit selected node.</button>
+<br/><button @click="deleteNode">Delete selected node.</button><br/>
+<br/><button @click="createBond">Create a bond.</button><br/>
+<p><button @click="editNode" class="editSelectedNode">Edit selected node.</button></p>
+<div id="Form" ref="Form">
 <form>
-<p><textarea id="edit" rows=20 cols=150 ref="edit"></textarea></p>
+<br/>
+<label>id: </label>
+<input value="" ref="idInp">
+<p>
+    <label>type: </label>
+    <input value="" ref="typeInp">
+</p>
+<p>
+    <label>service: </label>
+    <input value="" ref="serviceInp">
+</p>
+<p>
+    <label>callback: </label>
+    <input value="" ref="callbackInp">
+</p>
+<p>
+    <label>inputs:</label><br/><br/>
+        <label>name: </label>
+        <input value="" ref="inputsNameInp"><br/><br/>
+        <label>value: </label>
+        <input value="" ref="inputsValueInp"><br/><br/>
+        <label>type: </label>
+        <input value="" ref="inputsTypeInp">
+</p>
+<p>
+    <label>outputs:</label><br/><br/>
+        <label>name: </label>
+        <input value="" ref="outputsNameInp"><br/><br/>
+        <label>value: </label>
+        <input value="" ref="outputsValueInp"><br/><br/>
+        <label>type: </label>
+        <input value="" ref="outputsTypeInp">
+</p>
+<br/>
 </form>
-<button @click="confirmChngs" id="cnfChg" ref="cnfChg">Confirm changes.</button>
+</div>
+<p><button @click="confirmChanges" id="cnfChg" ref="cnfChg">Confirm changes.</button></p>
 </div>
 
 </template>
@@ -33,35 +69,61 @@ export default{
           color: '#9CEE90'
         }
       },
-      CurrentIndex: 0,
+      currentIndex: 0,
+      previewIndex: 0,
+      editingIndex: 0,
+      oldValue: 0,
       selected: false
   }),
   methods: {
     deleteNode(){
          this.nodes.splice(this.currentIndex, 1);
         },
+    createBond(){
+        this.edges.push({from:this.nodes[this.previewIndex].id, to:this.nodes[this.currentIndex].id});
+    },
     editNode(){
     if (this.selected){
+        this.editingIndex=this.currentIndex;
         this.$refs.cnfChg.style.display="block";
-        this.$refs.edit.style.display="block";
-        this.$refs.edit.value=JSON.stringify(this.nodes[this.currentIndex]);
+        this.$refs.Form.style.display="block";
+        this.$refs.idInp.value=this.nodes[this.editingIndex].id;
+        this.$refs.typeInp.value=this.nodes[this.editingIndex].type;
+        this.$refs.serviceInp.value=this.nodes[this.editingIndex].service;
+        this.$refs.callbackInp.value=this.nodes[this.editingIndex].callback;
+        this.$refs.inputsNameInp.value=this.nodes[this.editingIndex].inputs[0].name;
+            var editStrEdgBond = this.nodes[this.editingIndex].inputs[0].value.slice(5);
+                 var editPos = 0;
+                 editPos = editStrEdgBond.indexOf("::");
+                 editStrEdgBond = editStrEdgBond.slice(0,editPos);
+        this.$refs.inputsValueInp.value=editStrEdgBond;
+        this.oldValue=editStrEdgBond;
+        this.$refs.inputsTypeInp.value=this.nodes[this.editingIndex].inputs[0].type;
+        this.$refs.outputsNameInp.value=this.nodes[this.editingIndex].outputs[0].name;
+        this.$refs.outputsValueInp.value=this.nodes[this.editingIndex].outputs[0].value;
+        this.$refs.outputsTypeInp.value=this.nodes[this.editingIndex].outputs[0].type;
         }
     },
-    confirmChngs(){
+    confirmChanges(){
         this.$refs.cnfChg.style.display="none";
-        this.$refs.edit.style.display="none";
-        this.nodes[this.currentIndex]=JSON.parse(this.$refs.edit.value);
-        this.edges.splice(0,this.edges.length);
-        for (let i=0;i<this.nodes.length;i++){
-                    var strEdgBond = " ";
-                    if (this.nodes[i].inputs[0].value.indexOf("ref::")==0){
-                            strEdgBond = this.nodes[i].inputs[0].value.slice(5);
-                            var pos = 0;
-                            pos = strEdgBond.indexOf("::");
-                            strEdgBond = strEdgBond.slice(0,pos);
-                            this.edges.push({from: this.nodes[i].id, to:strEdgBond});
-                    }
-               }
+        this.$refs.Form.style.display="none";
+        var strEdgBond = 0;
+        strEdgBond = this.$refs.inputsValueInp.value;
+        this.nodes[this.editingIndex].inputs[0].value="ref::"+strEdgBond+"::out";
+        for (var i=0;i<this.edges.length;i++){
+            if (this.edges[i].from==this.nodes[this.editingIndex].id && this.edges[i].to==this.oldValue){
+                this.edges.splice(i,1);
+            }
+        }
+        this.edges.push({from: this.nodes[this.editingIndex].id, to:strEdgBond});
+        this.nodes[this.editingIndex].type=this.$refs.typeInp.value;
+        this.nodes[this.editingIndex].service=this.$refs.serviceInp.value;
+        this.nodes[this.editingIndex].callback=this.$refs.callbackInp.value;
+        this.nodes[this.editingIndex].inputs[0].name=this.$refs.inputsNameInp.value;
+        this.nodes[this.editingIndex].inputs[0].type=this.$refs.inputsTypeInp.value;
+        this.nodes[this.editingIndex].outputs[0].name=this.$refs.outputsNameInp.value;
+        this.nodes[this.editingIndex].outputs[0].value=this.$refs.outputsValueInp.value;
+        this.nodes[this.editingIndex].outputs[0].type=this.$refs.outputsTypeInp.value;
     },
     addNewTask(){
        this.nodes.splice(0,this.nodes.length);
@@ -86,14 +148,44 @@ export default{
                     this.edges.push({from: this.nodes[i].id, to:strEdgBond});
             }
        }
+       for (let i=0;i<this.nodes.length;i++){
+                       var currentId=this.nodes[i].id;
+                       if (i!=0){
+                           this.nodes[i].id=Math.ceil(Math.random() * (2500 - 1) + 1);
+                       }
+                       else{
+                           this.nodes[0].id=Math.ceil(Math.random() * (2500 - 1) + 1);
+                       }
+                       var newId=this.nodes[i].id;
+                       for (let j=0;j<this.edges.length;j++){
+                           if (this.edges[j].from==currentId){
+                               this.edges[j].from=newId;
+                           }
+                           if (this.edges[j].to==currentId){
+                               this.edges[j].to=newId;
+                           }
+                       }
+                       for (let f=0;f<this.nodes.length;f++){
+                           if (this.nodes[f].inputs[0].value!=""){
+                               var strEdgBond2 = this.nodes[f].inputs[0].value.slice(5);
+                               var pos2 = 0;
+                               pos2 = strEdgBond2.indexOf("::");
+                               strEdgBond2 = strEdgBond2.slice(0,pos2);
+                               if (strEdgBond2==currentId){
+                                     strEdgBond2=newId;
+                               }
+                               this.nodes[f].inputs[0].value="ref::"+strEdgBond2+"::out";
+                            }
+                       }
+                  }
     }
   },
   mounted(){
   var vm = this;
-  function onloadCallback(callback){
+  function onLoadCallback(callback){
   callback();
   }
-  onloadCallback(function(){
+  onLoadCallback(function(){
   var Tasks = [
                     {id:"0",  type: '', service: '', callback: '', inputs: [{name: '', value: 'ref::1::out', type: ''}], outputs: [{name: '', value: '', type: ''}]},
                     {id:"1",  type: '', service: '', callback: '', inputs: [{name: '', value: '', type: ''}], outputs: [{name: '', value: '', type: ''}]},
@@ -114,6 +206,36 @@ export default{
                         vm.edges.push({from: vm.nodes[i].id, to:strEdgBond});
                 }
            }
+           for (let i=0;i<vm.nodes.length;i++){
+                var currentId=vm.nodes[i].id;
+                if (i!=0){
+                    vm.nodes[i].id=Math.ceil(Math.random() * (2500 - 1) + 1);
+                }
+                else{
+                    vm.nodes[0].id=Math.ceil(Math.random() * (2500 - 1) + 1);
+                }
+                var newId=vm.nodes[i].id;
+                for (let j=0;j<vm.edges.length;j++){
+                    if (vm.edges[j].from==currentId){
+                        vm.edges[j].from=newId;
+                    }
+                    if (vm.edges[j].to==currentId){
+                        vm.edges[j].to=newId;
+                    }
+                }
+                for (let f=0;f<vm.nodes.length;f++){
+                    if (vm.nodes[f].inputs[0].value!=""){
+                        var strEdgBond2 = vm.nodes[f].inputs[0].value.slice(5);
+                        var pos2 = 0;
+                        pos2 = strEdgBond2.indexOf("::");
+                        strEdgBond2 = strEdgBond2.slice(0,pos2);
+                        if (strEdgBond2==currentId){
+                            strEdgBond2=newId;
+                        }
+                        vm.nodes[f].inputs[0].value="ref::"+strEdgBond2+"::out";
+                        }
+                }
+           }
   });
   document.onclick = function(e){
       if ( e.target.className == 'editSelectedNode') {
@@ -122,6 +244,7 @@ export default{
   }
   this.$refs.network.network.on('click', function (properties){
     vm.selected=true;
+    vm.previewIndex=vm.currentIndex;
     for (let i=0; i<vm.nodes.length; i++){
             if (properties.nodes[0]==vm.nodes[i].id){
                 vm.currentIndex=i;
@@ -143,11 +266,15 @@ export default{
   height: 75vh;
   width: auto;
 }
-#edit{
+#Form{
 display: none;
 margin-left: auto;
 margin-right: auto;
 background-color: #424242;
+color: white;
+}
+input{
+background-color: #576981;
 color: white;
 }
 #cnfChg{
